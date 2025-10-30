7c930ab63779433acb7814d18a285ed043c5f9c0
Fff-dot <daripandra@gmail.com>
2025-10-30 17:08:06 +0700

    undo

--- render-start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-composer install --no-dev --optimize-autoloader
-php artisan key:generate --force
-php artisan config:cache
-php artisan migrate --force
-php artisan serve --host 0.0.0.0 --port 10000
